3a253b8ab5bd351473b04320a156f411d118e33f
solcr7 <jacozha@gmx.de>
2019-03-20 18:24:05 +0100

    Tackling Accessibility

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = "Indoor Photo of the Restaurant"
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
